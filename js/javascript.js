75812ec6c678e842d7ae0dee9d2c49f52b28a19d
Janine Derix <j.derix@wbl-konzept.de>
2014-06-01 22:00:28 +0200

    commit all

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Janine Derix
+ * Date: 23.04.14
+ * Time: 15:22
+ * To change this template use File | Settings | File Templates.
+ */
+function buttonClick(aa) {
+ alert('HI' + aa);
+}
