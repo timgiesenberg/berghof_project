b00254a8e21637946b3283b53393c93cfd769aa2
Tim Giesenberg <tim.giesenberg@me.com>
2014-08-29 16:44:34 +0200

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,3 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: Janine Derix
- * Date: 23.04.14
- * Time: 15:22
- * To change this template use File | Settings | File Templates.
- */
 function buttonClick(id) {
     var div = document.getElementById(id);
     
